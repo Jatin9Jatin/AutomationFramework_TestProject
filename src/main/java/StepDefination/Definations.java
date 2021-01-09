8cb7db9f989495e93aec3e2a5c8c0dadf24b25f0
ShiviPrabhakar <58953132+ShiviPrabhakar@users.noreply.github.com>
2021-01-08 20:16:48 -0500

    updating Jars and Adding Steps

--- src/main/java/StepDefination/Definations.java
+++ src/main/java/StepDefination/Definations.java
@@ -1,4 +1,27 @@
 package StepDefination;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Definations {
+
+    @Given("precondition landing on facebook webpage")
+    public void precondition_landing_on_facebook_webpage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Enter username and password")
+    public void enter_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("validation that i am lodgein")
+    public void validation_that_i_am_lodgein() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
